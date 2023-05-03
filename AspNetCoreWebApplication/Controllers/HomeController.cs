0caae46f8cc4b23f5b8a52d347f42a4b08c0706d
Tina <Tina_su@epam.com>
2023-05-03 22:08:38 +0800

    test

---
+++ AspNetCoreWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Tina just created a ASP.Net Core web application!";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            ViewData["Message"] = "We've encountered an error :(";
+            return View();
+        }
+
+        public IActionResult BadCode()
+        {
+            try
+            {
+                ViewData["Message"] = "Test try catch block";
+                return View();
+            }
+            catch
+            {
+                return Error();
+            }            
+        }
+    }
+}
